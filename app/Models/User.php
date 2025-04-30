d4e16cfe7839d6d626b771dada36d8ac86e6d5c9
muqtafi <ahmadmuqtafi58@gmail.com>
2025-04-30 18:34:22 +0700

    add users relationships

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
     ];
 
     /**
@@ -44,5 +45,17 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function destinasi () {
+        return $this->hasMany(Destinasi::class, 'user_id', 'id');
+    }
+
+    public function bookmakrs () {
+        return $this->hasMany(Bookmark::class, 'user_id', 'id');
+    }
+
+    public function reviews () {
+        return $this->hasMany(Review::class, 'user_id', 'id');
     }
 }
