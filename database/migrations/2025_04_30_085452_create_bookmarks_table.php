7c22cd5741fccd48ccd14e46e3b6184f0e5dbd74
muqtafi <ahmadmuqtafi58@gmail.com>
2025-04-30 15:56:22 +0700

    add bookmark model and migration

---
+++ database/migrations/2025_04_30_085452_create_bookmarks_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bookmarks', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("user_id");
+            $table->foreign("user_id")->on("users")->onDelete("cascade");
+            $table->unsignedBigInteger("destinasi_id");
+            $table->foreign("destinasi_id")->on("destinasis")->onDelete("cascade");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bookmarks');
+    }
+};
